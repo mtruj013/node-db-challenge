8e765f399b4e79f33aeaef330d5154dbfe128c12
Maria Trujillo <mptruj015@gmail.com>
2020-05-15 20:09:10 +0200

    finished resource endpoints

--- projects/resources-router.js
+++ projects/resources-router.js
@@ -4,7 +4,7 @@ const Projects = require('./projects-model.js')
 
 const router = express.Router();
 
-router.get('/resources', (req,res) => {
+router.get('/', (req,res) => {
     Projects.getResources()
     .then(resources => {
         res.json(resources)
@@ -12,6 +12,18 @@ router.get('/resources', (req,res) => {
     .catch(err => {
         res.status(500)
         .json({ message: "Unable to get resource" })
+    })
+})
+
+router.post('/', (req,res) => {
+    Projects.addResource(req.body)
+    .then(resource => {
+        res.status(201)
+        .json(resource)
+    })
+    .catch(err => {
+        res.status(500)
+        .json({ message: "unable to add resource"})
     })
 })
 
--- server.js
+++ server.js
@@ -2,12 +2,13 @@ const express = require('express')
 const helmet = require('helmet')
 
 const projectsRouter = require('./projects/projects-router.js')
-
+const resourcesRouter = require('./projects/resources-router.js')
 const server = express()
 
 server.use(helmet())
 server.use(express.json())
 
 server.use('/api/projects', projectsRouter)
+server.use('/api/projects/resources', resourcesRouter)
 
 module.exports = server;
