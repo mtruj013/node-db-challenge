89fcd4ac3eb57804c12724de4059de66280be1ac
Maria Trujillo <mptruj015@gmail.com>
2020-05-15 19:22:27 +0200

    finished model

---
+++ projects/projects-model.js
@@ -0,0 +1,47 @@
+const db = require('../data/db-Config.js');
+
+module.exports ={
+    getProjects,
+    getResources,
+    getTasks,
+    addProject,
+    addResource,
+    addTask
+}
+
+//gets all projects
+function getProjects(){
+    return db("projects")
+}
+
+
+//gets resources
+function getResources(){
+    return db("resources")
+}
+
+//gets tasks
+function getTasks(id){
+    return db("tasks")
+    .select("projects.project_name", "project.project_description")
+    .join("projects", "tasks.project_id", "projects.id")
+    .where("projects.id", id)
+}
+
+//adds project
+function addProject(project){
+    return db("projects")
+    .insert(project, "id")
+}
+
+//adds resource
+function addResource(resource){
+    return db("resources")
+    .insert(resource, "id")
+}
+
+//adds tasks
+function addTask(task, project_id){
+    return db("tasks")
+    .insert(task, project_id)
+}
